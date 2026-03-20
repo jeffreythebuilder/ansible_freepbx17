6f88ca1144eed2167c5a021fb5441d842652019e
jeffreythebuilder <jeff08568@live.com>
2026-03-20 10:54:48 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+set -e
 
+echo "Installing Ansible..."
+apt-get update -y
+apt-get install -y ansible git
+
+echo "Cloning playbook..."
+git clone https://github.com/jeffreythebuilder/ansible_freepbx17.git /root/playbook
+
+echo "Running playbook..."
+ansible-playbook /root/playbook/site.yml
